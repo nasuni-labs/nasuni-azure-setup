9550f5bb52057119b6c4a338f4cce8baf65a7d5d
Pradeepta Sahu <psahu@nasuni.com>
2023-10-06 11:41:54 +0530

    conditionally create pem key

--- variables.tf
+++ variables.tf
@@ -4,89 +4,103 @@ variable "user-vnet-name" {
   default     = ""
 }
 
-variable "pem-key-path" {
-  description = "Key Name with extension and location"
+variable "user_subnet_name" {
+  description = "Subnet name from Virtual Network"
   type        = string
   default     = ""
 }
 
 variable "cred-vault" {
-  type    = string
-  default = ""
-}
-
-variable "destination-container-url" {
-  type    = string
-  default = ""
+  description = "Credential Key Vault Name"
+  type        = string
+  default     = ""
 }
 
 variable "github-organization" {
-  type    = string
-  default = "nasuni-labs"
+  description = "GitHub Organization Name"
+  type        = string
+  default     = "psahuNasuni"
 }
 
 variable "nac-scheduler-name" {
-  type    = string
-  default = ""
+  description = "NACScheduler VM Name"
+  type        = string
 }
 
 variable "edgeappliance-resource-group" {
-  type    = string
-  default = ""
+  description = "Resource Group Name where All Edge Resources are Provisioned"
+  type        = string
 }
 
 variable "nmc-api-endpoint" {
-  type    = string
-  default = ""
+  description = "IP Address of NMC"
+  type        = string
 }
 
 variable "nmc-api-password" {
-  type    = string
-  default = ""
+  description = "Passworf for NMC Login"
+  type        = string
 }
 
 variable "nmc-api-username" {
-  type    = string
-  default = ""
+  description = "User Name for NMC Login"
+  type        = string
 }
 
 variable "user-vault-name" {
-  type    = string
-  default = ""
+  description = "Name of User Input Key Vault"
+  type        = string
+  default     = ""
 }
 
 variable "product-key" {
-  type    = string
-  default = ""
-}
-
-variable "sp-application-id" {
-  type    = string
-  default = ""
+  description = "Product Key"
+  type        = string
 }
 
 variable "sp-secret" {
-  type    = string
-  default = ""
+  description = "Application secret of the Service Principal"
+  type        = string
+}
+
+variable "sp-application-id" {
+  description = "Application ID of the Service Principal"
+  type        = string
 }
 
 variable "use-private-ip" {
-  type    = string
-  default = "N"
+  description = "To Provision the Infrastrure in Private or Public Network"
+  type        = string
+  default     = "N"
+}
+
+variable "create-pem-key" {
+  description = "Set to true to create a PEM key, false otherwise"
+  type        = bool
+  default     = false
 }
 
 variable "networking-resource-group" {
-  type    = string
-  default = ""
-}
-
-variable "volume-key-container-url" {
-  type    = string
-  default = ""
+  description = "Resource Group Name where Vnet is exist"
+  type        = string
 }
 
 variable "web-access-appliance-address" {
-  type    = string
-  default = ""
+  description = "IP Address of Filer"
+  type        = string
 }
 
+variable "pgp-key-path" {
+  description = "Key Name with extension and location"
+  type        = string
+}
+
+variable "root-user" {
+  description = "Root User Name"
+  type        = string
+}
+
+variable "root-password" {
+  description = "Root User Password"
+  type        = string
+}
